980d5a98720a4e7ac7833a7ed3a510fe2bfd185c
Samuel Bonney <github.grinning329@silomails.com>
2025-06-05 14:04:37 +0100

    Remove elfeed-tube

--- init.el
+++ init.el
@@ -325,22 +325,6 @@
             (nreverse channels)))
   ))
 
-(use-package elfeed-tube
-  :ensure t
-  :after elfeed
-  :demand t
-  :config
-  ;; (setq elfeed-tube-auto-save-p nil) ; default value
-  ;; (setq elfeed-tube-auto-fetch-p t)  ; default value
-  (elfeed-tube-setup)
-
-  :bind (:map elfeed-show-mode-map
-         ("F" . elfeed-tube-fetch)
-         ([remap save-buffer] . elfeed-tube-save)
-         :map elfeed-search-mode-map
-         ("F" . elfeed-tube-fetch)
-         ([remap save-buffer] . elfeed-tube-save)))
-
 (use-package titlecase
   :defer t)
 
