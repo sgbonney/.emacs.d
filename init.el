04e9a304c8720e7f94403cd1147b27fb7dff1eb5
Samuel Bonney <github.isotope341@passmail.net>
2025-09-18 14:14:12 +0100

    Update path for launch-keepassxc.sh to .local/bin

--- init.el
+++ init.el
@@ -343,7 +343,7 @@
   (interactive)
   (let ((command "am startservice --user 0 -n com.termux/com.termux.app.RunCommandService \
 -a com.termux.RUN_COMMAND \
---es com.termux.RUN_COMMAND_PATH '/data/data/com.termux/files/home/bin/launch-keepassxc.sh' \
+--es com.termux.RUN_COMMAND_PATH '/data/data/com.termux/files/home/.local/bin/launch-keepassxc.sh' \
 --esa com.termux.RUN_COMMAND_ARGUMENTS '' \
 --es com.termux.RUN_COMMAND_WORKDIR '/data/data/com.termux/files/home' \
 --ez com.termux.RUN_COMMAND_BACKGROUND 'true' \
